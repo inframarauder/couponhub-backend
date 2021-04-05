7fd18f22c369fbd0c4ec5944768a104e68cb6818
subhasis020299 <subhasisdas125@gmail.com>
2021-04-05 17:19:36 +0530

    added schema level validations to models

--- models/user.model.js
+++ models/user.model.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     },
     name: {
       type: String,
-      required: true,
+      required: [true, "Name is required"],
     },
     credits: {
       type: Number,
